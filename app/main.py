367333e58871aa7c08e8f988ecf2655410499ab0
Kally95 <kally-95@hotmail.com>
2025-04-02 01:12:45 +0100

    extract URL path

--- app/main.py
+++ app/main.py
@@ -5,13 +5,17 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
+
     server_socket = socket.create_server(("localhost", 4221))
     conn, _ = server_socket.accept() 
-    conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    data = conn.recv(4096)
+    res = data.decode("utf-8").split("/")[1].split(' ')[0]
+    if(not res):
+        conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
     
-
+    else:
+        conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    
 
 if __name__ == "__main__":
     main()
