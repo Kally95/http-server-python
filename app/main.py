9fd4a6a06bd254a56a10b92609b73d036eaf182e
Kally95 <kally-95@hotmail.com>
2025-04-02 13:17:02 +0100

    respond with body

--- app/main.py
+++ app/main.py
@@ -12,14 +12,19 @@ def main():
     res = data.decode("utf-8").split("/")[2].split(' ')[0]
     content_length = len(list(res))
     REQUESTED_ENDPOINT = data.decode("utf-8").split("/")[1]
-    API_ENDPOINT = "/echo/"
-    print(data)
+    print(data.decode().split('\r\n'))
     print(res)
+    lines = data.decode().split('\r\n')
+    host_line = next((line for line in lines if line.lower().startswith("host:")), None)
     if(REQUESTED_ENDPOINT == "echo"):
         formatted_response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len}\r\n\r\n{res}".format(res=res, len=content_length)
         conn.sendall(formatted_response.encode("utf-8"))
+    elif (host_line):
+        host = host_line.split(":", 1)[1].strip()
+        conn.sendall(b"HTTP/1.1 202 OK\r\n\r\n")
     else:
         conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+
         
     
 
