1845bad946b0f5a53f5e81be299946285b87c7ab
Kally95 <kally-95@hotmail.com>
2025-04-02 13:28:03 +0100

    respond with body task

--- app/main.py
+++ app/main.py
@@ -15,12 +15,12 @@ def main():
     print(data.decode().split('\r\n'))
     print(res)
     lines = data.decode().split('\r\n')
-    host_line = next((line for line in lines if line.lower().startswith("host:")), None)
+    host_line = next((line for line in lines if line.lower().startswith("get")), None)
+    REQUESTED_PATH = host_line.split(' ')[1]
     if(REQUESTED_ENDPOINT == "echo"):
         formatted_response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len}\r\n\r\n{res}".format(res=res, len=content_length)
         conn.sendall(formatted_response.encode("utf-8"))
-    elif (host_line):
-        host = host_line.split(":", 1)[1].strip()
+    elif (REQUESTED_PATH ==  "/echo/{string}".format(string=res)):
         conn.sendall(b"HTTP/1.1 202 OK\r\n\r\n")
     else:
         conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
