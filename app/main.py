c30957029316189642a282203a72c79e6425bd0b
Kally95 <kally-95@hotmail.com>
2025-04-02 15:50:22 +0100

    Read header task

--- app/main.py
+++ app/main.py
@@ -13,9 +13,7 @@ def main():
                 continue  
             request, headers = data.decode().split("\r\n", 1)
             method, target = request.split(" ")[:2]
-            print("splitting", data.decode().split("\r\n"))
-            user_agent = [string for string in data.decode().split("\r\n") if "User-Agent" in string][0].split(":")[1].strip()
-            print(user_agent)
+
             if not data:
                 break
             if target == "/":
@@ -24,6 +22,7 @@ def main():
                 value = target.split("/echo/")[1]
                 response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(value)}\r\n\r\n{value}".encode()
             elif target == "/user-agent":
+                user_agent = [string for string in data.decode().split("\r\n") if "User-Agent" in string][0].split(":")[1].strip()
                 response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}".encode()
             else:
                 response = b"HTTP/1.1 404 Not Found\r\n\r\n"
